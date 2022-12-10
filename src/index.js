318c3e81683978ba041c208d15829335ca36a1e2
Prashant Bhushan Shukla <59869762+prashantshukla2000@users.noreply.github.com>
2022-12-11 03:20:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//Spread operator for Array
+// const citrus = ["Lemon", "Lime", "Orange"];
+// const fruit = ["Apple", "Coconut", "Banana", ...citrus];
+// console.log(fruit);
+
+//Spread operator for objects
+
+// const fullName={
+//   fname:"James",
+//   lName:"Bond"
+// };
+
+// const user1={
+//   ...fullName,
+//   id:1,
+//   userName:"JamesBond007"
+// };
+// console.log(user1);
